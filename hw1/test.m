53ab87366c10a07c4057562c81ec20707f7d41db
stalem9 <stalem9@gmail.com>
2020-11-02 20:49:12 +0300

    first commit

--- hw1/test.m
+++ hw1/test.m
@@ -1,22 +1,42 @@
 %% tests
 
 %% E2A
-test1_e2a = RTmatrices.Rx(pi/2)*RTmatrices.Ry(pi/4)*RTmatrices.Rx(pi/2)
+test1_e2a = RTmatrices.Rx(pi/2)*RTmatrices.Ry(pi/4)*RTmatrices.Rx(pi/2);
 Q1 = radtodeg(E2A(test1_e2a))
 
-test2_e2a = RTmatrices.Rx(pi/4)*RTmatrices.Ry(pi/2)*RTmatrices.Rx(pi)
+test2_e2a = RTmatrices.Rx(pi/4)*RTmatrices.Ry(pi/2)*RTmatrices.Rx(pi);
 Q2 = radtodeg(E2A(test2_e2a))
 
-test3_e2a = RTmatrices.Rx(pi/4)*RTmatrices.Ry(pi)*RTmatrices.Rx(pi)
+test3_e2a = RTmatrices.Rx(pi/4)*RTmatrices.Ry(pi)*RTmatrices.Rx(pi);
 Q3 = radtodeg(E2A(test3_e2a))
 
-test4_e2a = RTmatrices.Rx(pi/4)*RTmatrices.Ry(-pi)*RTmatrices.Rx(pi)
+test4_e2a = RTmatrices.Rx(pi/4)*RTmatrices.Ry(-pi)*RTmatrices.Rx(pi);
 Q4 = radtodeg(E2A(test4_e2a))
 
-test5_e2a = RTmatrices.Rx(pi/6)*RTmatrices.Ry(-pi/2)*RTmatrices.Rx(pi)
+test5_e2a = RTmatrices.Rx(pi/6)*RTmatrices.Ry(-pi/2)*RTmatrices.Rx(pi);
 Q5 = radtodeg(E2A(test5_e2a))
 
 %% FK - IK
 q1 = pi/2; q2 = pi/2; q3 = pi/2; q4 = pi/2; q5 = pi/4; q6 = pi/2;
-fk = FK(q1, q2, q3, q4, q5, q6)
-ik = radtodeg(IK(fk))
+fk = FK(q1, q2, q3, q4, q5, q6);
+ik1 = radtodeg(IK(fk));
+
+q1 = pi/4; q2 = pi/6; q3 = -pi/3; q4 = pi/2; q5 = -pi/4; q6 = -2*pi;
+fk = FK(q1, q2, q3, q4, q5, q6);
+ik2 = radtodeg(IK(fk))
+
+q1 = -pi/4; q2 = pi/6; q3 = pi/3; q4 = pi/2; q5 = -pi/4; q6 = -2*pi;
+fk = FK(q1, q2, q3, q4, q5, q6);
+ik3 = radtodeg(IK(fk));
+
+q1 = 3*pi/4; q2 = pi/6; q3 = pi/3; q4 = pi/2; q5 = -pi/4; q6 = -2*pi;
+fk = FK(q1, q2, q3, q4, q5, q6);
+ik4 = radtodeg(IK(fk));
+
+q1 = pi/4; q2 = -pi/6; q3 = pi/3; q4 = pi/2; q5 = -pi/4; q6 = -2*pi;
+fk = FK(q1, q2, q3, q4, q5, q6);
+ik5 = radtodeg(IK(fk));
+
+q1 = pi/4; q2 = pi/2; q3 = pi/3; q4 = pi/2; q5 = -pi/4; q6 = -2*pi;
+fk = FK(q1, q2, q3, q4, q5, q6);
+ik6 = radtodeg(IK(fk));                                 
