5d3791fa5ba01b3791e8ce6b43971710e6bb6e24
stalem9 <stalem9@gmail.com>
2020-11-27 15:15:51 +0300

    delete not used files

--- hw2/my_robot/src/state_publisher.cpp
+++ hw2/my_robot/src/state_publisher.cpp
@@ -48,32 +48,32 @@
    	   joint1 += tinc;
 	   loop_rate.sleep();
 	   }
-	   while(ros::ok() && joint3 > 0.85){
+	  // while(ros::ok() && joint3 > 0.85){
 	   //update joint_state
-           joint_state.header.stamp = ros::Time::now();
-           joint_state.name.resize(3);
-           joint_state.position.resize(3);
-           joint_state.name[0] ="joint1";
-           joint_state.position[0] = joint1;
-           joint_state.name[1] ="joint2";
-           joint_state.position[1] = joint2;
-           joint_state.name[2] ="joint3";
-           joint_state.position[2] = joint3;
+          // joint_state.header.stamp = ros::Time::now();
+          // joint_state.name.resize(3);
+          // joint_state.position.resize(3);
+          // joint_state.name[0] ="joint1";
+          // joint_state.position[0] = joint1;
+          // joint_state.name[1] ="joint2";
+          // joint_state.position[1] = joint2;
+          // joint_state.name[2] ="joint3";
+           //joint_state.position[2] = joint3;
 
 	   // update transform
            // (moving in a circle with radius=2)
-           odom_trans.header.stamp = ros::Time::now();
-           odom_trans.transform.translation.x = cos(angle)*2;
-           odom_trans.transform.translation.y = sin(angle)*2;
-           odom_trans.transform.translation.z = .7;
-           odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(angle+M_PI/2);
+           //odom_trans.header.stamp = ros::Time::now();
+           //odom_trans.transform.translation.x = cos(angle)*2;
+           //odom_trans.transform.translation.y = sin(angle)*2;
+           //odom_trans.transform.translation.z = .7;
+           //odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(angle+M_PI/2);
            
 	   //send the joint state and transform
-           joint_pub.publish(joint_state);
-           broadcaster.sendTransform(odom_trans);
-   	   joint1 -= tinc;
-	   loop_rate.sleep();
-	   }
+           //joint_pub.publish(joint_state);
+           //broadcaster.sendTransform(odom_trans);
+   	   //joint3 -= tinc;
+	   //loop_rate.sleep();
+	   //}
 	
    
    
